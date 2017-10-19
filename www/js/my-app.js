7c002f54328a3eefb83a463f5d507d17e508b7df
Kuan-Wen Liu <j70915@gmail.com>
2017-10-19 23:03:42 +0800

    add error test

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -55,6 +55,16 @@ const EXP_PER_LEVEL = 50;
 
 $$(document).on('deviceready', () => {
   console.log('Device is ready!');
+  function onSuccess(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+  }
+
+  function onError() {
+    console.log('err');
+  }
+  navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true });
+
 
   var applaunchCount = window.localStorage.getItem('launchCount');
   if (!applaunchCount) {
@@ -965,7 +975,7 @@ myApp.onPageInit('themeSite', () => {
     console.log('err');
     createSites(stations, favoriteSequence, -1, -1, onclickFunc);
   }
-  navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+  navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true });
   console.log('end of');
 });
 
@@ -1067,7 +1077,7 @@ myApp.onPageInit('favorite', () => {
   function onError() {
     createFavorite(itemList, -1, -1, onclickFunc);
   }
-  navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+  navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true });
 });
 
 myApp.onPageInit('customRoute', () => {
@@ -1108,7 +1118,7 @@ myApp.onPageInit('customRoute', () => {
   function onError() {
     createFavoriteCards(itemList, -1, -1, deleteFunc);
   }
-  navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+  navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true });
 
   mainView.showToolbar();
   $$('.toolbar').html('<div class="toolbar-inner"><a href="#" class="button button-big toolbar-text" style="text-align:center; margin:0 auto; height:48px;">確定行程</a></div>');
