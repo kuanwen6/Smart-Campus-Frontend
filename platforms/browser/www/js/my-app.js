fda9bbffeb7dc8af94491610da722070a2170ae7
winonecheng <F74036328@mail.ncku.edu.tw>
2017-10-20 07:52:25 +0800

    Move const definition to sparate constants.js file

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,15 +1,3 @@
-const myApp = new Framework7({
-  template7Pages: true, // enable Template7 rendering for Ajax and Dynamic pages
-  swipeBackPage: false,
-});
-
-const $$ = Dom7;
-
-const mainView = myApp.addView('.view-main', {
-  // Because we use fixed-through navbar we can enable dynamic navbar
-  dynamicNavbar: true,
-});
-
 $$(document).on('page:init', (e) => {
   const page = e.detail.page;
   console.log(page);
@@ -17,31 +5,11 @@ $$(document).on('page:init', (e) => {
 
 mainView.hideToolbar();
 
-const mySwiper = myApp.swiper('.swiper-container', {
-  pagination: '.swiper-pagination',
-});
 
-const welcomescreenSlides = [{
-    id: 'slide0',
-    picture: '<img src="../img/intro/intro_1.png">',
-  },
-  {
-    id: 'slide1',
-    picture: '<img src="../img/intro/intro_2.png">',
-  },
-  {
-    id: 'slide2',
-    picture: '<img src="../img/intro/intro_3.png">',
-    text: '<a id="welcome-close-btn" href="#"><img src="../img/intro/start_btn.png"></a>',
-  },
-];
-
-const HOOKURL = 'https://smartcampus.csie.ncku.edu.tw/';
 var directionsService;
 var directionsDisplay;
 
-// experience per level
-const EXP_PER_LEVEL = 50;
+
 
 $$(document).on('deviceready', () => {
   console.log('Device is ready!');
@@ -243,8 +211,8 @@ myApp.onPageInit('map', (page) => {
   };
 
   map.addListener('click', hideMarkerInfo);
-  setMarkers(map);
   directionOrMapOverview(page.context.isDirection);
+  setMarkers(map);
   navigator.geolocation.watchPosition(onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
 
   function directionOrMapOverview(isDirection) {
