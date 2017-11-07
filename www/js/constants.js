f357cfa9e269b73057aa07cb19dbd29c271c086f
winonecheng <F74036328@mail.ncku.edu.tw>
2017-11-08 00:41:23 +0800

    test

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,6 +1,6 @@
 var myApp = new Framework7({
   template7Pages: true, // enable Template7 rendering for Ajax and Dynamic pages
-  swipeBackPage: false
+  swipeBackPage: false,
   //preloadPreviousPage: false
 });
 
