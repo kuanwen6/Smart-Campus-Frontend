6ae39cafce86e2d8e99200dd6c72c6134b1dea72
winonecheng <F74036328@mail.ncku.edu.tw>
2017-11-07 23:02:14 +0800

    Reinstall statusbar plugin

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -51,20 +51,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
-        "id": "cordova-plugin-statusbar.StatusBarProxy",
-        "pluginId": "cordova-plugin-statusbar",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
         "pluginId": "cordova-plugin-splashscreen",
@@ -118,6 +104,20 @@ module.exports = [
             "navigator.notification",
             "navigator"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -126,13 +126,13 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.7",
     "com.unarin.cordova.beacon": "3.5.2",
-    "cordova-plugin-statusbar": "2.2.3",
     "cordova-plugin-splashscreen": "4.0.3",
     "cordova-custom-config": "4.0.2",
     "cordova-plugin-geolocation": "2.4.3",
     "cordova-plugin-compat": "1.2.0",
     "cordova-plugin-network-information": "1.3.3",
-    "cordova-plugin-vibration": "2.1.5"
+    "cordova-plugin-vibration": "2.1.5",
+    "cordova-plugin-statusbar": "2.3.1-dev"
 }
 // BOTTOM OF METADATA
 });
