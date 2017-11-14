bbaea4c2dadbb48c746a2556a0d9f79849a876c7
jeffwang0516 <jeff1jeffo@gmail.com>
2017-11-15 00:31:48 +0800

    Let user select whether to receive notification in bg

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -27,13 +27,19 @@ $$(document).on('backbutton', function() {
 });
 
 $$(document).on('pause', function() {
-  //beacon_util.stopScanForBeacons();
+  if (localStorage.getItem("bgDetect") == null) return;
+  if (localStorage.getItem("bgDetect") !== "true") {
+    beacon_util.stopScanForBeacons();
+  }
 
   console.log("pause");
 });
 
 $$(document).on('resume', function() {
-  //beacon_util.startScanForBeacons();
+  if (localStorage.getItem("bgDetect") == null) return;
+  if (localStorage.getItem("bgDetect") !== "true") {
+    beacon_util.startScanForBeacons();
+  }
 
   console.log("resume");
 });
@@ -75,6 +81,14 @@ $$(document).on('deviceready', function() {
           closeButton: false,
           onClosed: function() {
             beacon_util.startUpBeaconUtil();
+            myApp.confirm('是否開啟背景偵測？在您未使用時亦會顯示接近站點通知', '接近站點通知',
+              function() {
+                window.localStorage.setItem('bgDetect', true);
+              },
+              function() {
+                window.localStorage.setItem('bgDetect', false);
+              }
+            );
           },
         }
       );
